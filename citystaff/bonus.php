9bdafc5fb272be2d17dae1903be864e281673b87
iamgodwater@gmail.com <iamgodwater@gmail.com>
2025-09-21 12:22:51 +0400

    исправление сообщения модального окна и фотографии на баннере

--- citystaff/bonus.php
+++ citystaff/bonus.php
@@ -321,7 +321,6 @@ $APPLICATION->SetTitle("бонус");
 );?>
     </section>
 
-   
     <!--бонусы инфо-->
     <section class="widthBase NB_content NB1_section_info NB_content widthBase d-flex flex-row justify-content-between">
         <div class="NB2_bonusInfo_left d-flex flex-column justify-content-between">
@@ -517,6 +516,39 @@ $APPLICATION->SetTitle("бонус");
         );?>
         </div>
     </section>
+
+    <?php
+        $showClass = '';
+        $showMonkey = 'display: none;';
+        $showRequest = 'display: flex;';
+        if(isset($_GET['ref_sent'])) {
+            if (strip_tags($_GET['ref_sent']) == "success") {
+                $showClass = "NB_active";
+                $showRequest = 'display: none;';
+                $showMonkey = 'display: flex;';
+            }
+            else if (strip_tags($_GET['ref_sent']) == "fail") {
+                $showClass = "NB_active";
+            }
+        }
+        ?>
+    <!--модальное окно-->
+    <section class="NB_modal d-flex flex-row align-items-center justify-content-center <?=$showClass?>" id="application">
+        <div class="NB_modal_item d-flex flex-column align-items-center">
+            <img alt="X" src="<?=SITE_TEMPLATE_PATH?>/assets/img/X.svg" class="NB_modal_item_X close_modal">
+            <div class="NB_modal_item_item flex-column align-items-center" id="survey" style="<?=$showRequest?>">
+                <h4>Заполните имя и контакт друга, чтобы мы могли учесть заявку</h4>
+                <script data-b24-form="inline/25/5wmkc3" data-skip-moving="true">(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://stage.citystaff.asgardd.tech/upload/crm/form/loader_25_5wmkc3.js');</script>
+                <p class="Fa NB_modal_item_footer">Нажимая на кнопку «Отправить», вы даёте согласие на обработку персональных данных.</p>
+            </div>
+            <div class="NB_modal_item_item flex-column align-items-center" id="happyMonkey" style="<?=$showMonkey?>">
+                <h4>Заявка принята!</h4>
+                <p class="Fbody NB_modal_item_p">Мы начислим 5 000 ₽ после выхода вашего друга на работу</p>
+                <img src="<?=SITE_TEMPLATE_PATH?>/assets/img/monkey.png" alt="" class="NB_modal_monkey">
+            </div>
+        </div>
+    </section>
+
 
     <!--стаффлики-->
     <section data-nav-title="Как заработать стаффлики?" id="staffcoinList" class="widthBase NB_content NB2_stafcoin d-flex flex-column">
