3ff31eca07820c83172ea5dcc5c4f90e8f22a731
iamgodwater@gmail.com <iamgodwater@gmail.com>
2025-09-22 12:19:37 +0400

    новая форма (обратная связь)

--- citystaff/faq.php
+++ citystaff/faq.php
@@ -223,7 +223,7 @@ $APPLICATION->SetTitle("помощь");
                 );?>
             </div>
             <div class="NB3_banner_form d-flex flex-column align-items-center justify-content-between NB_rounded-20">
-                <script data-b24-form="inline/25/5wmkc3" data-skip-moving="true">(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://stage.citystaff.asgardd.tech/upload/crm/form/loader_25_5wmkc3.js');</script>
+                <script data-b24-form="inline/26/ydymm1" data-skip-moving="true">(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://stage.citystaff.asgardd.tech/upload/crm/form/loader_26_ydymm1.js');</script>
             </div>
         </div>
     </section>
